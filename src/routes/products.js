8b514889d4b8527c0da09b5e0d202b594405772a
Maximiliano G Ramos <maximiliano.ramos23@gmail.com>
2023-03-20 18:30:01 -0300

    method delete and put

--- src/routes/products.js
+++ src/routes/products.js
@@ -17,4 +17,30 @@ router.get('/products', (req, res) => {
         .catch((error) => res.json({ message: error }))
 })
 
+router.get('/products/:id', (req, res) => {
+    const { id } = req.params
+    productsSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+})
+
+router.delete('/products/:id', (req, res) => {
+    const { id } = req.params
+    productsSchema
+        .deleteOne({_id: id})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+})
+
+router.put('/products/:id', (req, res) => {
+    const { id } = req.params
+    const { name, category, ubication, price } = req.body
+    productsSchema
+        .updateOne({_id:id}, { $set: { name, category, ubication, price }})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+        
+})
+
 module.exports = router
